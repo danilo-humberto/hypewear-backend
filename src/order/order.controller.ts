ba8de32e5422ad41a2bfbee7c14cc51a9df31545
Leandro <149272684+leandroxzq@users.noreply.github.com>
2025-10-28 14:30:14 -0300

    order rules

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, NotFoundException, ParseIntPipe } from '@nestjs/common';
+import { OrderService } from './order.service';
+import { CreateOrderDto } from './dto/create-order.dto';
 
-@Controller('order')
-export class OrderController {}
+@Controller('orders')
+export class OrderController {
+  constructor(private readonly orderService: OrderService) {}
+
+  @Get()
+  findAll() {
+    return this.orderService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.orderService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() createOrderDto: CreateOrderDto) {
+    return this.orderService.create(createOrderDto);
+  }
+
+  @Post(':id/confirm-payment')
+  confirmPayment(@Param('id', ParseIntPipe) id: number) {
+    return this.orderService.confirmPayment(id);
+  }
+}
