80f4272daecad52c4fa1630bd9f3531d3a06cee8
juliana <user.taniajuju91@gmail.com>
2025-10-30 18:42:08 -0300

    FEAT: Uuid Payments

--- src/payments/payments.controller.ts
+++ src/payments/payments.controller.ts
@@ -1,39 +1,46 @@
-import { Controller, Post, Patch, Body,Param, Get, HttpCode, HttpStatus, UseGuards} from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Patch,
+  Body,
+  Param,
+  Get,
+  HttpCode,
+  HttpStatus,
+  ParseUUIDPipe
+} from '@nestjs/common';
 import { PaymentService } from './payments.service';
 import { CreatePaymentDto } from './dto/create-payments.dto';
 
 @Controller('payments')
 export class PaymentController {
-    constructor(private readonly paymentService: PaymentService) {}
+  constructor(private readonly paymentService: PaymentService) {}
 
-    @Post()
-    @HttpCode(HttpStatus.CREATED)
-    createPayment(@Body() createPaymentDto: CreatePaymentDto) {
-        return this.paymentService.createPayment(createPaymentDto);
-    }
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  createPayment(@Body() createPaymentDto: CreatePaymentDto) {
+    return this.paymentService.createPayment(createPaymentDto);
+  }
 
-    @Patch(':id/confirm')
-    @HttpCode(HttpStatus.OK)
-    confirmPayment(@Param('id') id: string) {
-        const paymentId = parseInt(id, 10); 
-        return this.paymentService.confirmPayment(paymentId);
-    }
-    
-    @Patch(':id/cancel')
-    @HttpCode(HttpStatus.OK)
-    cancelPayment(@Param('id') id: string) {
-        const paymentId = parseInt(id, 10); 
-        return this.paymentService.cancelPayment(paymentId);
-    }
+  @Patch(':id/confirm')
+  @HttpCode(HttpStatus.OK)
+  confirmPayment(@Param('id', ParseUUIDPipe) id: string) {
+    return this.paymentService.confirmPayment(id);
+  }
 
-    @Get(':id')
-    findOne(@Param('id') id: string) {
-        const paymentId = parseInt(id, 10);
-        return this.paymentService.findOne(paymentId);
-    }
+  @Patch(':id/cancel')
+  @HttpCode(HttpStatus.OK)
+  cancelPayment(@Param('id', ParseUUIDPipe) id: string) {
+    return this.paymentService.cancelPayment(id);
+  }
 
-    @Get()
-    findAll() {
-        return this.paymentService.findAll();
-    }
+  @Get(':id')
+  findOne(@Param('id', ParseUUIDPipe) id: string) {
+    return this.paymentService.findOne(id);
+  }
+
+  @Get()
+  findAll() {
+    return this.paymentService.findAll();
+  }
 }
